d2baafb7ef4d4729f362c94cc958ce310e7c1c36
Suraj <maharjansuraj286@gmail.com>
2025-05-22 13:10:10 +0545

    commit

--- JavaScript/content.js
+++ JavaScript/content.js
@@ -1,29 +1,27 @@
 console.log("content");
-let count=0;
-//let page="all";
-  function idElement(idData){
-  const container=document.getElementById(idData);
-  const filteredProducts = products.filter(product => product.category === idData);
-  count=0;
-  filteredProducts.forEach(product => {
-    
-    if((page=="all")|| (count<3 && page=="main")){
-    const card = document.createElement("div");
-    card.className = "grid-item";
-    /**
-    for checking and to overview only
-     */
-    card.style.backgroundImage = product.image ? `url(${product.image})` : "none";
-    card.style.backgroundColor = "black"; 
-    
-    card.innerHTML = `
-  <h3>${product.name}</h3>
-  <div class="card-footer">
-    <p class="text-container">Price: $${product.price}</p>
-    <button onclick="addCart(${product.id})">Add to Cart</button>
-  </div>
-`;
+let count = 0;
 
-    container.appendChild(card);
-    count ++;
-    }})};
+function idElement(idData) {
+  const container = document.getElementById(idData);
+  const filteredProducts = products.filter(product => product.category === idData);
+  count = 0;
+
+  filteredProducts.forEach(product => {
+    if ((page === "all") || (count < 3 && page === "main")) {
+      const card = document.createElement("div");
+      card.className = "grid-item";
+
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.name}" class="product-img" />
+        <h3>${product.name}</h3>
+        <div class="card-footer">
+          <p class="text-container">Price: $${product.price}</p>
+          <button onclick="addCart(${product.id})">Add to Cart</button>
+        </div>
+      `;
+
+      container.appendChild(card);
+      count++;
+    }
+  });
+}
